c4f6b48d8614bed6cf95b972971a7b7a34d3393f
liaolebing <1744098752@qq.com>
2020-11-22 22:00:11 +0800

    Add assign2

--- app/Controllers/KoreanGirlGroup.php
+++ app/Controllers/KoreanGirlGroup.php
@@ -1,80 +1,79 @@
-<?php namespace App\Controllers;
-
-class KoreanGirlGroup extends BaseController
+<?php namespace AppControllers;
+class KoreanGirlGroup  extends BaseController
 {
     public function index()
     {
-        // connect to the model
-    $places = new \App\Models\member();
-        // retrieve all the records
-    $records = $places->findAll();
+         //connect to the model
+    $member = new AppModelsmember();
+         //retrieve all the records
+    $records = $member-findAll();
     
-        // get a template parser
-    $parser = \Config\Services::parser();
-        // tell it about the substitions
-    $table = new \CodeIgniter\View\Table();
+         //get a template parser
+    $parser = ConfigServicesparser();
+         //tell it about the substitions
+    $table = new CodeIgniterViewTable();
     
-    $headings = $Member->fields;
-    $displayHeadings = array_slice($headings, 1, 2); 
-    $table->setHeading(array_map('ucfirst', $displayHeadings));
     
-    foreach ($records as $record) 
+    $headings=$member-fields;
+    $table-setHeading($headings[1],$heading[7],details);
+    
+     foreach ($records as $record) 
     {
-        $nameLink = anchor("KoreanGirlGroup/showme/$record->id",$record->name);
-        $table->addRow($nameLink,$record->description);
+        $nameLink = anchor(KoreanGirlGroupshowme$record-id ,"details");
+        $table-addRow($nameLink,$record-description);
     }
     
     $template = [
-                'table_open' => '<table cellpadding="5px">',
-                'cell_start' => '<td style="border: 1px solid #dddddd;">',
-                'row_alt_start' => '<tr style="background-color:#dddddd">',
+                'table_open' = 'table cellpadding=5px',
+                'cell_start' = 'td style=border 1px solid #dddddd;',
+                'row_alt_start' = 'tr style=background-color#dddddd',
                 ];
-    $table->setTemplate($template);
+    $table-setTemplate($template);
     
    $fields = [
-       'title' => 'Travel Destinations',
-       'heading' => 'Travel Destinations',
-       'footer' => 'lyx'];
-  
-    return $parser->setData($fields)->render('templates\top') .
-           $table->generate().
-           $parser->setData($fields)->render('templates\bottom');
-    
+       'title' = 'Travel Destinations',
+       'heading' = 'Travel Destinations',
+       'footer' = 'lyx'];
+   return $parser-setData($fields)-render('templatestop') .
+           $table-generate().
+           $parser-setData($fields)-render('templatesbottom');
     }
     public function showme($id)
     {
-        // connect to the model
-      $places = new \App\Models\member();
-        // retrieve all the records
-      $record = $places->find($id);
-      // get a template parser
-      $parser = \Config\Services::parser();
-      // tell it about the substitions
-      
-       $table = new \CodeIgniter\View\Table();
+         connect to the model
+      $member = new AppModelsmember();
+         retrieve all the records
+      $record = $member-find($id);
+       get a template parser
+      $parser = ConfigServicesparser();
+       tell it about the substitions
+      $table = new CodeIgniterViewTable();
     
-    $headings = $member->fields;
-    $table->addRow($headings[0],$record['id'])
-          ->addRow($headings[1],$record['name'])
-          ->addRow($headings[2],$record['description'])
-          ->addRow($headings[3],$record['link'])
-          ->addRow($headings[4],"<img src=\"/image/".$record['image']."\"/>");
+    $headings = $member-fields;
+    $table-addRow($headings[0],$record['id'])
+          -addRow($headings[1],$record['name'])
+          -addRow($headings[2],$record['country'])
+          -addRow($headings[3],$record['favorite food'])
+          -addRow($headings[4],img src=image.$record['image'].)
+          -addRow($headings[5],$record['hobby'])
+          -addRow($headings[6],$record['Favorite color'])
+          -addRow($headings[7],$record['company']);
 
     $template = [
-                'table_open' => '<table cellpadding="5px">',
-                'cell_start' => '<td style="border: 1px solid #dddddd;">',
-                'row_alt_start' => '<tr style="background-color:#dddddd">',
+                'table_open' = 'table cellpadding=5px',
+                'cell_start' = 'td style=border 1px solid #dddddd;',
+                'row_alt_start' = 'tr style=background-color#dddddd',
                 ];
-    $table->setTemplate($template);
+    $table-setTemplate($template);
     
    $fields = [
-       'title' => 'Travel Destinations',
-       'heading' => 'Travel Destinations',
-       'footer' => 'yingxianLiao'
+       'title' = 'Travel Destinations',
+       'heading' = 'Travel Destinations',
+       'footer' = 'yingxianLiao'
        ];
       
-   return $parser->setData($fields)->render('templates\top') .
-           $table->generate().
-           $parser->setData($fields)->render('templates\bottom');
+     return $parser-setData($fields)-render('templatestop') .
+           $table-generate().
+           $parser-setData($fields)-render('templatesbottom');
     }
 }
